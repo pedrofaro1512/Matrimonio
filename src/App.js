86d3a237866aea5edcfa625df743f359a3af72ed
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-08-08 17:45:16 -0500

    Adición de sección momentos, regalos, formulario y footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
+import FooterM from "./Components/Footer/Footer";
 import Nosotros from "./Components/Nosotros/Nosotros";
 import Contador from "./Components/Contador/Contador";
 import Itinerario from "./Components/Itinerario/Itinerario";
@@ -9,6 +9,9 @@ import Comentarios from "./Components/Comentarios/Comentarios";
 import Home from "./Components/Home/Home";
 import Momentos from "./Components/Momentos/Momentos";
 import Vestimenta from "./Components/Vestimenta/Vestimenta";
+
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { FloatButton } from "antd";
 
 import "./App.css";
 
@@ -25,7 +28,16 @@ function App() {
       <Regalos />
       <Comentarios />
       <Confirmacion />
-      <Footer />
+      <FooterM />
+      <>
+        <FloatButton
+          icon={<QuestionCircleOutlined />}
+          type="primary"
+          style={{
+            right: 24,
+          }}
+        />
+      </>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Layout } from "antd";
+import "./Footer.css";
+
+const { Footer } = Layout;
+
+const FooterM = () => {
+  return (
+    <Footer className="footer">
+      <h1>Contáctanos</h1>
+      <div className="footer-content">
+        <div>
+          <h2>Natalia Cortes</h2>
+          <h3>Celulear: 312 0000000</h3>
+        </div>
+        <div>
+          <h2>Felipe Rodriguéz</h2>
+          <h3>Celular: 312 0000000</h3>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+
+export default FooterM;
