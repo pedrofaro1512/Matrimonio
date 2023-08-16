171db3600c1e05f471ec2642eab260fcaf4b189f
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-08-16 17:30:09 -0500

    Adición de musica e Iniciales

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Header.css";
 import { Menu } from "antd";
 import { Link } from "react-scroll";
+import Letras from "../../asstes/img/Letras03.png";
 
 const items = [
   {
@@ -30,21 +31,26 @@ const Header = () => {
 
   return (
     <div className="menu-container">
-      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
-        {items.map((item) => (
-          <Menu.Item key={item.key}>
-            <Link
-              to={item.key}
-              spy={true}
-              smooth={true}
-              duration={1500}
-              offset={-10}
-            >
-              {item.label}
-            </Link>
-          </Menu.Item>
-        ))}
-      </Menu>
+      <div className="letrasH">
+        <img src={Letras} alt="Letras"></img>
+      </div>
+      <div className="menu-items">
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
+          {items.map((item) => (
+            <Menu.Item key={item.key}>
+              <Link
+                to={item.key}
+                spy={true}
+                smooth={true}
+                duration={1500}
+                offset={-10}
+              >
+                {item.label}
+              </Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+      </div>
     </div>
   );
 };
